96340d649c104e0cf1e967852d4f341fe3738925
Danu <danu3006@gmail.com>
2019-07-04 15:25:29 +0100

    update: Electron renderer process

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,6 @@ import * as path from 'path';
 const application = app;
 let main: BrowserWindow | null;
 
-// const createMenu = () => {
-// 	const { Menu } = electron;
-// 	const template = [
-// 		{
-// 			label: 'Edit',
-// 			submenu: [
-// 				{ role: 'cut' },
-// 				{ role: 'copy' },
-// 				{ role: 'paste' },
-// 				{ role: 'selectall' }
-// 			]
-// 		},
-// 		{
-// 			label: 'View',
-// 			submenu: [
-// 				{ role: 'reload' },
-// 				{ role: 'forcereload' },
-// 				{ role: 'toggledevtools' }
-// 			]
-// 		}
-// 	];
-// 	const menu = Menu.buildFromTemplate(template);
-// 	Menu.setApplicationMenu(menu);
-// };
-
 function createWindow() {
 	const icon = nativeImage.createFromPath(
 		path.join(__dirname, 'src/assets/logo.png')
