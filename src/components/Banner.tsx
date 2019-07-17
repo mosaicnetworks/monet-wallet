87331fa0d566ca296b12ba7006ec4ab1e041bcb5
Danu <danu3006@gmail.com>
2019-07-17 11:29:02 +0100

    update: Banner to dynamic styled component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 
 const colors = {
 	purple: 'rgba(126, 66, 149, 0.9)',
@@ -7,6 +7,19 @@ const colors = {
 	black: '#222',
 	blue: 'rgba(24, 64, 150, 0.9)'
 };
+
+const BannerContainer = styled.div`
+	background: ${props => props.color} !important;
+	color: #fff !important;
+	padding: 20px;
+	box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3) !important;
+	position: relative;
+	-webkit-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3),
+		0 0 40px rgba(0, 0, 0, 0.1) inset;
+	-moz-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3),
+		0 0 40px rgba(0, 0, 0, 0.1) inset;
+	box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+`;
 
 interface Props {
 	color: 'purple' | 'orange' | 'black' | 'blue';
@@ -17,21 +30,17 @@ class Banner extends React.Component<Props, any> {
 	public render() {
 		const { color } = this.props;
 
-		const Banner = styled.div`
-			background: ${colors[color]} !important;
-			color: #fff !important;
-			padding: 20px;
-			box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3) !important;
-			position: relative;
-			-webkit-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3),
-				0 0 40px rgba(0, 0, 0, 0.1) inset;
-			-moz-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3),
-				0 0 40px rgba(0, 0, 0, 0.1) inset;
-			box-shadow: 0 1px 4px rgba(0, 0, 0, 0.3),
-				0 0 40px rgba(0, 0, 0, 0.1) inset;
-		`;
+		const theme = {
+			color
+		};
 
-		return <Banner style={this.props.style}>{this.props.children}</Banner>;
+		return (
+			<ThemeProvider theme={theme}>
+				<BannerContainer style={this.props.style}>
+					{this.props.children}
+				</BannerContainer>
+			</ThemeProvider>
+		);
 	}
 }
 
